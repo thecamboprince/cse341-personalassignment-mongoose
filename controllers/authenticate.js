3d9522cc4a0e9db03caf8e0e26133e33a2dd6ef9
thecamboprince <dug21002@byui.edu>
2024-02-23 13:31:37 -0700

    comments

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,11 +1,16 @@
+// Middleware function to check if the user is authenticated
 const authUserLogin = (req, res, next) => {
+  // Check if the user is not authenticated
   if (!req.oidc.isAuthenticated()) {
+    // If not authenticated, send a 401 Unauthorized status with an error message
     return res.status(401).send({
       error: "Unauthorized: Please login to make changes!",
       login: "You can log in at:  localhost:8080/login",
     });
   }
+  // If authenticated, call the next middleware function
   next();
 };
 
+// Export the middleware function to be used in other parts of the application
 module.exports = { authUserLogin };
